404cd5edecd195460cc0fa120bf288a3404aae81
Nico <596049368@qq.com>
2019-10-20 20:43:09 +0800

    Update qxapi.py

--- qxapi.py
+++ qxapi.py
@@ -23,12 +23,13 @@ def Retry_request(url): #远程下载
             i = i +1
             print('重新下载：'+url)
 
-def getrules(link,tag):             # 自定义规则
+def getrules(subs,tags):             # 自定义规则
     
     try:
         rule = Retry_request('https://raw.githubusercontent.com/lzdnico/SSRClash/master/qxconfig')        #请求规则_神机规则
         rules = str(rule).split('下方粘贴你的订阅链接')
-        rules[0] += '\n' + link + ', tag=' +  tag + ', enabled=true\n'
+        for i in range(len(subs)):
+            rules[0] += '\n' + subs[i] + ', tag=' +  tags[i] + ', enabled=true'
         return rules[0] + rules[1]
     except Exception as e:
         print(e)
@@ -37,14 +38,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def my():
-    return 'qx api 使用：调用地址为：http://ip/2333/订阅地址@tag。其中订阅地址中的/替换为!，tag为标签'
+    return 'qx api 使用：<br/>调用地址为：http://ip:2333/订阅地址@tag。其中订阅地址中的/替换为!，tag为标签<br/>'\
+            '将调用地址复制到qx——配置文件——下载。<br/>'\
+            '保存后，长按策略图标，给健康检测添加节点。将最优的节点排在前面。<br/>'
+
+
+#http://ip:2333/机场1@机场1标签@@机场2@机场2标签@@机场3@机场3标签
+#订阅地址中的/替换为！
+#@@分割机场         
+#机场标签可以不填，如果不填默认值为傻吊家的
 
 @app.route('/<name>',methods=['GET'])
 def get(name):
-    name = name.replace('!','/')    
-    link = name.split('@')[0]
-    tag =  name.split('@')[1]
-    return getrules(link,tag)
+    subs = []
+    tags = []
+
+    name = name.replace('!','/')
+    links = name.split('@@')
+    for link in links:
+        subs.append(link.split('@')[0])
+        try:
+            tags.append(link.split('@')[1])
+        except Exception as e:
+            tags.append('傻吊家的节点')    
+    return getrules(subs,tags)
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=False,port=2333)
